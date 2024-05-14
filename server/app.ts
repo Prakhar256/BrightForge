bcd00031f25b292c2635a8a5bd7356f492f6c7c8
Prakhar256 <prakharjain256@gmail.com>
2024-05-15 02:44:36 +0530

    Fetch last 12 months analytics

--- server/app.ts
+++ server/app.ts
@@ -8,6 +8,7 @@ import userRouter from "./routes/user.route"
 import courseRouter from "./routes/course.route"
 import orderRouter from "./routes/order.route";
 import notificationRouter from "./routes/notification.route"
+import analyticsRouter from "./routes/analytics.route";
 // body parser
 app.use(express.json({ limit: "50mb" }));
 
@@ -27,6 +28,7 @@ app.use("/api/v1",userRouter);
 app.use("/api/v1",courseRouter);
 app.use("/api/v1",orderRouter);
 app.use("/api/v1",notificationRouter);
+app.use("/api/v1",analyticsRouter);
 
 // Checking API
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
