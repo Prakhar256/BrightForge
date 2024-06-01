fc947c622a3944c60c6e846e81fc8852b1ba9e24
Prakhar256 <prakharjain256@gmail.com>
2024-06-01 17:18:24 +0530

    Admin dashboard completed !

--- client/app/components/Admin/DashboardHero.tsx
+++ client/app/components/Admin/DashboardHero.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import DashboardHeader from "./DashboardHeader";
-// import DashboardWidgets from "../../components/Admin/Widgets/DashboardWidgets";
+import DashboardWidgets from "../../components/Admin/Widgets/DashboardWidgets";
 
 type Props = {
   isDashboard?: boolean;
@@ -13,7 +13,9 @@ const DashboardHero = ({isDashboard}: Props) => {
     <div>
       <DashboardHeader open={open} setOpen={setOpen} />
       {
-        
+        isDashboard && (
+          <DashboardWidgets open={open} />
+        )
       }
     </div>
   );
---
+++ client/redux/features/orders/ordersApi.ts
@@ -0,0 +1,28 @@
+import { apiSlice } from "../api/apiSlice";
+
+export const ordersApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllOrders: builder.query({
+      query: (type) => ({
+        url: `get-orders`,
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+    
+    createOrder: builder.mutation({
+      query: ({ courseId, payment_info }) => ({
+        url: "create-order",
+        body: {
+          courseId,
+          payment_info,
+        },
+        method: "POST",
+        credentials: "include" as const,
+      }),
+    }),
+  }),
+});
+
+export const { useGetAllOrdersQuery, useCreateOrderMutation} =
+  ordersApi;
