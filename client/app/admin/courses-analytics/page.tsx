a2895db871b28d69ed4ab78d310b5005964ffc3d
Prakhar256 <prakharjain256@gmail.com>
2024-06-01 16:32:34 +0530

    Analytics pages Ui done

---
+++ client/app/admin/courses-analytics/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+import React from 'react'
+import AdminSidebar from "../../components/Admin/sidebar/AdminSidebar";
+import Heading from '../../utils/Heading';
+import CourseAnalytics from "../../components/Admin/Analytics/CourseAnalytics";
+import DashboardHeader from '../../components/Admin/DashboardHeader';
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+        <Heading
+          title="Bright Forge"
+          description="Bright Forge is a platform for students to learn coding and get expertise from industry level experts"
+          keywords="DSA, MERN stack development, Generative AI, Machine Learning"
+        />
+        <div className="flex">
+            <div className="1500px:w-[16%] w-1/5">
+                <AdminSidebar />
+            </div>
+            <div className="w-[85%]">
+               <DashboardHeader />
+               <CourseAnalytics />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default page
