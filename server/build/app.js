effce57a8005cd857738f659fef8443fc3c8bf0a
Prakhar256 <prakharjain256@gmail.com>
2024-06-07 22:43:24 +0530

    Final commit

--- server/build/app.js
+++ server/build/app.js
@@ -23,7 +23,7 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross origin resource sharing
 exports.app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000"],
+    origin: ["https://bright-forge-client-1.vercel.app"],
     credentials: true,
 }));
 // api requests limit
