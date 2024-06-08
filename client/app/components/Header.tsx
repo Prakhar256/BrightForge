791511a966e1256c0684da2a4c6ee412f54b622a
Prakhar256 <prakharjain256@gmail.com>
2024-06-09 01:44:40 +0530

    Profile pic error solved

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -109,7 +109,7 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
                   size={25}
                   className="cursor-pointer dark:text-white text-black"
                   onClick={() => setOpenSidebar(true)}
-                />
+                />  
               </div>
               {userData ? (
                 <Link href={"/profile"}>
@@ -125,7 +125,7 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
               ) : (
                 <HiOutlineUserCircle
                   size={25}
-                  className="hidden 800px:block cursor-pointer dark:text-white text-black"
+                  className="800px:block cursor-pointer dark:text-white text-black"
                   onClick={() => setOpen(true)}
                 />
               )}
@@ -142,14 +142,14 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
           >
             <div className="w-[70%] fixed z-[999999999] h-screen bg-white dark:bg-slate-900 dark:bg-opacity-90 top-0 right-0">
               <NavItems activeItem={activeItem} isMobile={true} />
-              {userData ? (
+              {userData?.user ? (
                 <Link href={"/profile"}>
                   <Image
                     src={userData?.user.avatar ? userData.user.avatar.url : avatar}
                     alt=""
                     width={30}
                     height={30}
-                    className="w-[30px] h-[30px] rounded-full cursor-pointer"
+                    className="w-[30px] h-[30px] rounded-full ml-[25px] mt-[10px] cursor-pointer"
                     style={{border: activeItem === 5 ? "2px solid #37a39a" : "none"}}
                   />
                 </Link>
